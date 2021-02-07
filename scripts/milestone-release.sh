892e365d4e7d7794b146419d113674922eff3a7a
Håvard Ottestad <hmottestad@gmail.com>
2021-02-07 09:27:35 +0100

    milestone release script
    Signed-off-by: Håvard Ottestad <hmottestad@gmail.com>

--- scripts/milestone-release.sh
+++ scripts/milestone-release.sh
@@ -2,18 +2,6 @@
 
 cd ..
 
-increment_version() {
- local v=$1
- if [ -z $2 ]; then
-    local rgx='^((?:[0-9]+\.)*)([0-9]+)($)'
- else
-    local rgx='^((?:[0-9]+\.){'$(($2-1))'})([0-9]+)(\.|$)'
-    for (( p=$(grep -o "\."<<<".$v"|wc -l); p<$2; p++)); do
-       v+=.0; done; fi
- val=$(echo -e "$v" | perl -pe 's/^.*'$rgx'.*$/$2/')
- echo "$v" | perl -pe s/$rgx.*$'/${1}'$(printf %0${#val}s $(($val+1)))/
-}
-
 echo ""
 echo "The release script requires several external command line tools:"
 echo " - git"
@@ -78,21 +66,25 @@ fi
 
 # check that we are on master
 if  ! git status --porcelain --branch | grep -q "## master...origin/master"; then
-  echo""
-  echo "You need to be on master!";
-  echo "git checkout master";
-  echo "";
-  exit 1;
+  if  ! git status --porcelain --branch | grep -q "## develop...origin/develop"; then
+    echo""
+    echo "You need to be on master or develop!";
+    echo "";
+    exit 1;
+  fi
 fi
 
 echo "Running git pull to make sure we are up to date"
 git pull
 
 # check that we are not ahead or behind
-if  ! [[ $(git status --porcelain -u no  --branch) == "## master...origin/master" ]]; then
-    echo "";
+if  ! git status --porcelain --branch | grep -q "## master...origin/master"; then
+  if  ! git status --porcelain --branch | grep -q "## develop...origin/develop"; then
+    echo""
     echo "There is something wrong with your git. It seems you are not up to date with master. Run git status";
+    echo "";
     exit 1;
+  fi
 fi
 
 # check that there are no uncomitted or untracked files
@@ -115,15 +107,12 @@ mvn clean;
 
 MVN_CURRENT_SNAPSHOT_VERSION=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()" pom.xml)
 
-# replace "SNAPSHOT" with ""
-MVN_VERSION_RELEASE="${MVN_CURRENT_SNAPSHOT_VERSION/-SNAPSHOT/}"
-
-MVN_NEXT_SNAPSHOT_VERSION="$(increment_version "$MVN_VERSION_RELEASE" 3)-SNAPSHOT"
-
 echo "";
 echo "Your current maven snapshot version is: '${MVN_CURRENT_SNAPSHOT_VERSION}'"
+echo ""
+read -rp "What is the version you would like to release?" MVN_VERSION_RELEASE
+echo ""
 echo "Your maven release version will be: '${MVN_VERSION_RELEASE}'"
-echo "Your next maven snapshot version will be: '${MVN_NEXT_SNAPSHOT_VERSION}'"
 read -n 1 -srp "Press any key to continue (ctrl+c to cancel)"; printf "\n\n";
 
 # set maven version
@@ -167,61 +156,6 @@ echo "(if you are on linux or windows, remember to use CTRL+SHIFT+C to copy)."
 echo "Log in, then choose 'Build with Parameters' and type in ${MVN_VERSION_RELEASE}"
 read -n 1 -srp "Press any key to continue (ctrl+c to cancel)"; printf "\n\n";
 
-# Cleanup
-mvn clean
-
-# Set a new SNAPSHOT version
-echo "";
-echo "Setting the next snapshot version to: ${MVN_NEXT_SNAPSHOT_VERSION}"
-read -n 1 -srp "Press any key to continue (ctrl+c to cancel)"; printf "\n\n";
-
-
-# set maven version
-mvn versions:set -DnewVersion="${MVN_NEXT_SNAPSHOT_VERSION}"
-
-#Remove backup files. Finally, commit the version number changes:
-mvn versions:commit
-mvn -P compliance versions:commit
-
-echo "";
-echo "Committing the new version to git"
-git commit -s -a -m "next development iteration: ${MVN_NEXT_SNAPSHOT_VERSION}"
-echo "Pushing the new version to github"
-git push
-
-echo "";
-echo "About to create PR"
-read -n 1 -srp "Press any key to continue (ctrl+c to cancel)"; printf "\n\n";
-echo "";
-
-echo "Creating pull request to merge release branch back into master"
-gh pr create --title "next development iteration: ${MVN_NEXT_SNAPSHOT_VERSION}" --body "Merge using merge commit rather than rebase"
-
-echo "";
-echo "Preparing a merge-branch to merge into develop"
-read -n 1 -srp "Press any key to continue (ctrl+c to cancel)"; printf "\n\n";
-
-
-git checkout develop
-git pull
-
-MVN_VERSION_DEVELOP=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()" pom.xml)
-
-git checkout "${BRANCH}"
-
-git checkout -b "merge_master_into_develop_after_release_${MVN_VERSION_RELEASE}"
-mvn versions:set -DnewVersion="${MVN_VERSION_DEVELOP}"
-mvn versions:commit
-mvn -P compliance versions:commit
-git commit -s -a -m "set correct version"
-git push --set-upstream origin "merge_master_into_develop_after_release_${MVN_VERSION_RELEASE}"
-
-echo "Creating pull request to merge the merge-branch into develop"
-gh pr create -B develop --title "sync develop branch after release ${MVN_VERSION_RELEASE}" --body "Merge using merge commit rather than rebase"
-echo "It's ok to merge this PR later, so wait for the Jenkins tests to finish."
-read -n 1 -srp "Press any key to continue (ctrl+c to cancel)"; printf "\n\n";
-
-git checkout master
 mvn clean
 
 cd scripts
